88f2b12e4853955a7e6cca48d1e64a5f65e3cc13
vasanth12345 <vasanthvp1611@gmail.com>
2021-05-25 18:01:09 +0530

    js mini proj

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const bar = document.querySelector('.bar');
+const nav = document.querySelector('.navbar');
+
+bar.addEventListener('click', ()=> {
+ bar.classList.toggle('active'); 
+ nav.classList.toggle('navopen'); 
+});
+
